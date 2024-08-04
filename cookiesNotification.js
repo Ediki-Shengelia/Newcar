725835171c67a7026acb2a91822bb086d814df52
ediki <shengelia301@gmail.com>
2024-08-04 15:00:06 +0400

    1d

--- cookiesNotification.js
+++ cookiesNotification.js
@@ -3,12 +3,12 @@
 export function notification() {
   let cookieNotification = document.getElementById("cookie-notification");
   let acceptCookiesButton = document.getElementById("accept-cookies");
+
   // Check if user has already accepted cookies
   let cookiesAccepted = localStorage.getItem("cookieAccepted");
 
   if (!cookiesAccepted) {
     // Show the cookies notification
-
     cookieNotification.style.display = "block";
   }
 
@@ -17,6 +17,6 @@ export function notification() {
     // Hide the cookies notification
     cookieNotification.style.display = "none";
     // Set a flag in localStorage to remember that the user accepted cookies
-    localStorage.setItem("cookiesAccepted", true);
+    localStorage.setItem("cookieAccepted", true);
   });
 }
