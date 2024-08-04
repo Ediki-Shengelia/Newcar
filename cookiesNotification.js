4f5ce2784c093ea5133bd3bba344de8526bd11e5
ediki <shengelia301@gmail.com>
2024-08-04 15:07:15 +0400

    finish122

--- cookiesNotification.js
+++ cookiesNotification.js
@@ -1,22 +1,24 @@
 "use strict";
 
 export function notification() {
-  let cookieNotification = document.getElementById("cookie-notification");
-  let acceptCookiesButton = document.getElementById("accept-cookies");
+  document.addEventListener("DOMContentLoaded", function () {
+    var cookieNotification = document.getElementById("cookie-notification");
+    var acceptCookiesButton = document.getElementById("accept-cookies");
 
-  // Check if user has already accepted cookies
-  let cookiesAccepted = localStorage.getItem("cookieAccepted");
+    // Check if user has already accepted cookies
+    var cookiesAccepted = localStorage.getItem("cookiesAccepted");
 
-  if (!cookiesAccepted) {
-    // Show the cookies notification
-    cookieNotification.style.display = "block";
-  }
+    if (!cookiesAccepted) {
+      // Show the cookies notification
+      cookieNotification.style.display = "block";
+    }
 
-  // Event listener for accept cookies button
-  acceptCookiesButton.addEventListener("click", function () {
-    // Hide the cookies notification
-    cookieNotification.style.display = "none";
-    // Set a flag in localStorage to remember that the user accepted cookies
-    localStorage.setItem("cookieAccepted", true);
+    // Event listener for accept cookies button
+    acceptCookiesButton.addEventListener("click", function () {
+      // Hide the cookies notification
+      cookieNotification.style.display = "none";
+      // Set a flag in localStorage to remember that the user accepted cookies
+      localStorage.setItem("cookiesAccepted", true);
+    });
   });
 }
